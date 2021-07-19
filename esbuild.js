62afacd3b6c7ad8407d3555d8c0dde903f0037de
SWZ <contact@swz.works>
2021-07-19 20:01:41 +0200

    Added yare-sync support + Changed esbuild logging + Updated file watcher

--- esbuild.js
+++ esbuild.js
@@ -1,9 +1,17 @@
-let fs = require("fs");
-let path = require("path");
-let usingts = fs.existsSync(path.join(__dirname, "src/main.ts"));
-let usingjs = fs.existsSync(path.join(__dirname, "src/main.js"));
+const fs = require("fs");
+const path = require("path");
+const sync = require("yare-sync");
+const esbuild = require("esbuild");
+const watch = require("node-watch");
+const usingts = fs.existsSync(path.join(__dirname, "src/main.ts"));
+const usingjs = fs.existsSync(path.join(__dirname, "src/main.js"));
 
-let shouldwatch = process.argv.splice(2).includes("-w");
+require("colors");
+
+const flags = process.argv.splice(2);
+
+const shouldwatch = flags.includes("-w") || flags.includes("--watch");
+const shouldsync = flags.includes("-s") || flags.includes("--sync");
 
 if (!usingjs && !usingts) {
   console.log("You don't have a main file smh");
@@ -12,23 +20,50 @@ if (!usingjs && !usingts) {
 
 let mainfile = usingjs ? "src/main.js" : "src/main.ts";
 
-let watchoptions = shouldwatch
-  ? {
-      onRebuild(error, result) {
-        if (error) console.error("Build failed ):");
-        else console.log("Built successfully");
-      },
-    }
-  : undefined;
+const esbuildConfig = {
+  entryPoints: [mainfile],
+  bundle: true,
+  minify: true,
+  outfile: "dist/bundle.js",
+  treeShaking: true,
+  target: "es2015",
+};
 
-require("esbuild")
-  .build({
-    entryPoints: [mainfile],
-    bundle: true,
-    minify: true,
-    outfile: "dist/bundle.js",
-    treeShaking: true,
-    target: "es2015",
-    watch: watchoptions,
-  })
-  .catch(() => process.exit(1));
+function build() {
+  let result = esbuild.buildSync(esbuildConfig);
+
+  let code = fs.readFileSync(esbuildConfig.outfile, "utf-8");
+
+  if (result.errors.length > 0)
+    return console.error("Build failed ):".red.bold);
+  else console.log("Built successfully".green.bold);
+
+  if (!shouldsync) return;
+  sync
+    .updateCode(code)
+    .then(() => {
+      console.log("Uploaded to yare!".green.bold);
+    })
+    .catch((e) => {
+      console.error(
+        "Upload to yare failed. Do you a yare game open with the tampermonkey userscript?\nhttps://raw.githubusercontent.com/swz-gh/yare-sync/main/dist/client.js"
+          .red.bold
+      );
+    });
+}
+
+if (shouldwatch) {
+  build();
+  watch(
+    path.dirname(mainfile),
+    {
+      recursive: true,
+    },
+    (_, file) => {
+      console.log("File change".yellow, file);
+      build();
+    }
+  );
+} else {
+  build();
+}
