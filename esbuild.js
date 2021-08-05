cf63a0d8d562454982c5500cb04fe2640f0110c7
SWZ <contact@swz.works>
2021-08-05 16:44:18 +0200

    Switched to yare-sync v1.0

--- esbuild.js
+++ esbuild.js
@@ -1,17 +1,28 @@
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 const sync = require("yare-sync");
 const esbuild = require("esbuild");
 const watch = require("node-watch");
-const usingts = fs.existsSync(path.join(__dirname, "src/main.ts"));
-const usingjs = fs.existsSync(path.join(__dirname, "src/main.js"));
 
 require("colors");
+
+function input() {
+  return new Promise((r) => {
+    process.stdin.once("data", (d) => {
+      r(d);
+    });
+  });
+}
 
 const flags = process.argv.splice(2);
 
 const shouldwatch = flags.includes("-w") || flags.includes("--watch");
 const shouldsync = flags.includes("-s") || flags.includes("--sync");
+const shouldsync = flags.includes("-a") || flags.includes("--switch-acc");
+
+const usingts = fs.existsSync(path.join(__dirname, "src/main.ts"));
+const usingjs = fs.existsSync(path.join(__dirname, "src/main.js"));
 
 if (!usingjs && !usingts) {
   console.log("You don't have a main file smh");
@@ -29,7 +40,9 @@ const esbuildConfig = {
   target: "es2015",
 };
 
-function build() {
+let acc = null;
+
+async function build() {
   let result = esbuild.buildSync(esbuildConfig);
 
   let code = fs.readFileSync(esbuildConfig.outfile, "utf-8");
@@ -39,31 +52,70 @@ function build() {
   else console.log("Built successfully".green.bold);
 
   if (!shouldsync) return;
-  sync
-    .updateCode(code)
-    .then(() => {
-      console.log("Uploaded to yare!".green.bold);
-    })
-    .catch((e) => {
-      console.error(
-        "Upload to yare failed. Do you have a yare game open with the tampermonkey userscript?\nhttps://raw.githubusercontent.com/swz-gh/yare-sync/main/dist/client.user.js"
-          .red.bold
-      );
+  let games = await sync.getGames(acc.user_id);
+  let successful = await sync.sendCode(code, games, acc);
+  if (successful) {
+    console.log("Uploaded your code to these games:".green.bold, games);
+  } else {
+    console.error("Upload to yare failed.".red.bold);
+  }
+}
+function login() {
+  return new Promise(async (resolve) => {
+    console.log("Log in to yare to enable yare-sync".bold);
+    console.log("Username:");
+    let username = ((await input()) + "").split("\n")[0].split("\r")[0];
+    console.log("Password (SHOWN):");
+    let password = ((await input()) + "").split("\n")[0].split("\r")[0];
+    console.log("Trying to log in as".yellow, password);
+    let acc = sync.login(username, password).catch(async (e) => {
+      console.log("Invalid username or password, try again".red.bold);
+      resolve(await login());
     });
+    if (acc) resolve(acc);
+  });
 }
 
-if (shouldwatch) {
-  build();
-  watch(
-    path.dirname(mainfile),
-    {
-      recursive: true,
-    },
-    (_, file) => {
-      console.log("File change".yellow, file);
-      build();
+async function main() {
+  if (shouldsync) {
+    let savedSessionFilePath = path.join(
+      os.tmpdir(),
+      "yare-sync-last-session.json"
+    );
+    if (fs.existsSync(savedSessionFilePath)) {
+      let savedSessionFile = JSON.parse(
+        fs.readFileSync(savedSessionFilePath, "utf-8")
+      );
+      console.log("Found previous session".blue);
+      if (sync.verifySession(savedSessionFile)) {
+        console.log("Session was valid! Using that".green);
+        acc = savedSessionFile;
+      } else {
+        console.log("Invalid session".red);
+      }
     }
-  );
-} else {
-  build();
+    if (acc === null) {
+      acc = await login();
+    }
+    console.log("Logged in as".green.bold, acc.user_id, "\n");
+    fs.writeFileSync(savedSessionFilePath, JSON.stringify(acc), "utf-8");
+  }
+
+  if (shouldwatch) {
+    await build();
+    watch(
+      path.dirname(mainfile),
+      {
+        recursive: true,
+      },
+      (_, file) => {
+        console.log("File change".yellow, file);
+        build();
+      }
+    );
+  } else {
+    await build();
+  }
 }
+
+main();
