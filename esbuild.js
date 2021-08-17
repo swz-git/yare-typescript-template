cdfb634cf482a4119c2f88c34f22cc23c6f9e73c
swz-gh <contact@swz.works>
2021-08-17 23:49:39 +0200

    added -nm flag

--- esbuild.js
+++ esbuild.js
@@ -20,6 +20,7 @@ const flags = process.argv.splice(2);
 const shouldwatch = flags.includes("-w") || flags.includes("--watch");
 const shouldsync = flags.includes("-s") || flags.includes("--sync");
 const switchacc = flags.includes("-a") || flags.includes("--switch-acc");
+const nominify = flags.includes("-nm") || flags.includes("--no-minify");
 
 const usingts = fs.existsSync(path.join(__dirname, "src/main.ts"));
 const usingjs = fs.existsSync(path.join(__dirname, "src/main.js"));
@@ -34,7 +35,7 @@ let mainfile = usingjs ? "src/main.js" : "src/main.ts";
 const esbuildConfig = {
   entryPoints: [mainfile],
   bundle: true,
-  minify: true,
+  minify: !nominify,
   outfile: "dist/bundle.js",
   treeShaking: true,
   target: "es2015",
@@ -55,7 +56,10 @@ async function build() {
   let games = await sync.getGames(acc.user_id);
   let successful = await sync.sendCode(code, games, acc);
   if (successful) {
-    console.log("Uploaded your code to these games:".green.bold, games.map(g=>g?`${g.server}/${g.id}`:g));
+    console.log(
+      "Uploaded your code to these games:".green.bold,
+      games.map((g) => (g ? `${g.server}/${g.id}` : g))
+    );
   } else {
     console.error("Upload to yare failed.".red.bold);
   }
